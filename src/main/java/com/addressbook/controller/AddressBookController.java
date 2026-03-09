ac5583b9c8ca805c8f0ce764a0070a681af61e9c
priyanshu-kumar-2511 <kumarpriyanshu77828@gmail.com>
2026-03-09 11:41:41 +0530

    [Priyanshu] : UC2: Add AddressBook Contact

--- src/main/java/com/addressbook/controller/AddressBookController.java
+++ src/main/java/com/addressbook/controller/AddressBookController.java
@@ -1,14 +1,34 @@
 package com.addressbook.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.*;
+
+import com.addressbook.model.Contact;
 
 @RestController
+@RequestMapping("/addressbook")
 public class AddressBookController {
+
+    private List<Contact> contactList = new ArrayList<>();
 
     @GetMapping("/")
     public String addressBookApp() {
         return "Welcome to Address Book Application";
     }
 
+    @PostMapping("/add")
+    public String addContact(@RequestBody Contact contact) {
+
+        contactList.add(contact);
+
+        return "Contact added successfully";
+    }
+
+    @GetMapping("/contacts")
+    public List<Contact> getAllContacts() {
+
+        return contactList;
+    }
 }
--- src/main/java/com/addressbook/model/Contact.java
+++ src/main/java/com/addressbook/model/Contact.java
@@ -101,4 +101,14 @@ public class Contact {
     public void setEmail(String email) {
         this.email = email;
     }
+
+    @Override
+    public String toString() {
+        return "Contact{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", city='" + city + '\'' +
+                '}';
+    }
 }
